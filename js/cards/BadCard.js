c290993ccefc201780ce51894a7e69a511cccc9f
InsonusK <insonus.k@gmail.com>
2020-10-21 22:36:20 +0300

    Refactor card code. Close #14

---
+++ js/cards/BadCard.js
@@ -0,0 +1,47 @@
+class BadCard extends Card{
+    /**
+     *
+     * @param scene
+     * @param value
+     * @param cardTexture {CardTexture}
+     */
+    constructor(scene, value, cardTexture) {
+        super(scene, value, cardTexture);
+    }
+
+    onClick(){
+        super.onClick();
+        this.scene.lock = true;
+
+        this.scene.statistic.IncrementErrors();
+        this.scene.life.Reduce();
+        this.scene.scoreText.Update();
+        if (!this.life.IsAlive()) {
+            this.scene.sounds.timeout.play();
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Вы проиграли',
+                footer: '<a href="#">Не повезло в картах, повезет в любви =)</a>',
+                allowOutsideClick: false,
+                willClose: () => {
+                    this.scene.endGame(true);
+                    this.scene.lock = false;
+                }
+            })
+
+        } else {
+            Swal.fire({
+                text: `Появилась новая опасность, у вас осталось попыток: ${this.scene.life.currentLife}`,
+                allowOutsideClick: false,
+                willClose: () => {
+                    this.scene.lock = false;
+                }
+            });
+        }
+        if (this.scene.openedCard) {
+            this.scene.openedCard.close();
+        }
+        this.scene.openedCard = this;
+    }
+}
